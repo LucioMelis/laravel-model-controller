4115442a58f9176af30c8d9f6756e2aa23f89d94
luciomelis <luciomelis1992@gmail.com>
2022-05-20 16:30:40 +0200

    punto 7 (con errori/ orrori)

--- laravel-model-controller/app/Http/Controllers/PageController.php
+++ laravel-model-controller/app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 
     public function index(){
         $movies = Movie::all();
-        return view('home');
+        return view('home', compact('movies'));
     }
 
 }
