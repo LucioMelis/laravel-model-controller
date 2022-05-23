f2834f978c4c665bf72ceb78ad4904e9866ab8fe
luciomelis <luciomelis1992@gmail.com>
2022-05-23 20:35:06 +0200

    modifiche layout con inserimento immagini switch

--- laravel-model-controller/app/Movie.php
+++ laravel-model-controller/app/Movie.php
@@ -24,31 +24,31 @@ class Movie extends Model
                 return 'https://www.cincincinema.it/wp-content/uploads/2017/10/Il-Padrino.jpg';
                 break;
             case 2:
-                echo "i equals 1";
+                return 'https://m.media-amazon.com/images/I/51FvKgUQ1lL.jpg';
                 break;
             case 3:
-                echo "i equals 2";
+                return 'https://www.hollywood-video.it/components/com_virtuemart/shop_image/product/PSYCO_A.Hitchcoc_4f3fbe78b2445.png';
                 break;
             case 4:
-                echo "i equals 2";
+                return 'https://pad.mymovies.it/filmclub/2010/09/146/locandina.jpg';
                 break;
             case 5:
-                echo "i equals 2";
+                return 'https://pad.mymovies.it/filmclub/2006/08/330/locandinapg1.jpg';
                 break;
             case 6:
-                echo "i equals 2";
+                return 'https://m.media-amazon.com/images/I/71563g-IMbL._AC_SY606_.jpg';
                 break;
             case 7:
-                echo "i equals 2";
+                return 'https://pad.mymovies.it/filmclub/2006/08/130/locandina.jpg';
                 break;
             case 8:
-                echo "i equals 2";
+                return 'https://www.cinemadelsilenzio.it/images/film/poster/699_big.jpg';
                 break;
             case 9:
-                echo "i equals 2";
+                return 'https://pad.mymovies.it/filmclub/2006/05/253/locandina.jpg';
                 break;
             case 10:
-                echo "i equals 2";
+                return 'https://pad.mymovies.it/filmclub/2006/08/058/locandina.jpg';
                 break;
         }
     }
